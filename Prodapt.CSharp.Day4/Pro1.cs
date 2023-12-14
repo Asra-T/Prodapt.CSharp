dde00f1c329cf4d297da488b069cb166e7e7a6e9
Asra-T <asrabalkeesmohideen@gmail.com>
2023-12-14 11:05:26 +0530

    Add project files.

---
+++ Prodapt.CSharp.Day4/Pro1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Prodapt.CSharp.Library;
+namespace Prodapt.CSharp.Day4
+{
+    internal class Pro1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!!This ia asra");
+            Login obj = new Login();
+            var result=obj.Validation("admin", "admin@123");
+            Console.WriteLine(result ? "Valid User" : "Invalid User");
+
+        }
+    }
+}
---
+++ Prodapt.CSharp.Day5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Prodapt.CSharp.Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Now;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Today : {today}");
+            Console.WriteLine($"Dateonly : {today.ToShortDateString()}");
+            Console.WriteLine($"Dateonly : {today.ToShortDateString()}");
+        }
+    }
+}
